d2047fd77e8fd5f49926a5dca9841aaf010e72c2
BarasaJnr <isaacbarasa714@gmail.com>
2023-05-01 03:06:49 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,83 +1,45 @@
 #include "lists.h"
 
 /**
- * listint_len - Returns the number of nodes in a list
- * @h: The head of the list
+ * insert_nodeint_at_index - inserts a new node in a linked list,
+ * at a given position
+ * @head: pointer to the first node in the list
+ * @idx: index where the new node is added
+ * @n: data to insert in the new node
  *
- * Return: The number of nodes or 0
- */
-size_t listint_len(const listint_t *h)
-{
-	size_t numElem = 0;
-
-	while (h != NULL)
-	{
-		numElem++;
-		h = h->next;
-	}
-
-	return (numElem);
-}
-
-/**
- * get_nodeint_at_index - Returns the number at a particular node index
- * @head: Head of the list
- * @index: Index No of node
- *
- * Return: Pointer to the node
- */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
-{
-	size_t size = listint_len(head);
-	listint_t *node = head;
-	size_t current = 0;
-
-	if (index > size || head == NULL)
-		return (NULL);
-
-	while (current < index)
-	{
-		node = node->next;
-		current++;
-	}
-	return (node);
-}
-
-/**
- * insert_nodeint_at_index - Inserts a new node at index specified
- * @head: Pointer to head
- * @idx: The index position to replace
- * @n: The number for the node
- *
- * Return: The address of the new node
+ * Return: pointer to the new node, or NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *node;
-	listint_t *before;
-	size_t size;
+	unsigned int i;
 	listint_t *new;
-
-	if (head == NULL || *head == NULL)
-		return (NULL);
-
-	node = get_nodeint_at_index(*head, idx);
-	before = *head;
-	size = listint_len(*head);
-
-	if (idx > size)
-		return (NULL);
+	listint_t *temp = *head;
 
 	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	if (!new || !head)
 		return (NULL);
-	new->n = n;
 
-	while (before->next != node)
+	new->n = n;
+	new->next = NULL;
+
+	if (idx == 0)
 	{
-		before = before->next;
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
-	new->next = node;
-	before->next = new;
-	return (new);
+
+	for (i = 0; temp && i < idx; i++)
+	{
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
+	}
+
+	return (NULL);
 }
